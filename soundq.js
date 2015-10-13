201013e8326402b34a630262fb67eee64ebfe336
wreckingadm <wreckingadm@users.noreply.github.com>
2015-10-12 21:24:47 -0400

    Initial file structure
    Pretty much empty files, but soon to be stubs.

---
+++ soundq.js
@@ -0,0 +1,24 @@
+/*!
+ * SoundQ - 
+ * Queued audio playback with priority and channel support for the web
+ * https://github.com/wreckingadm/soundq-js
+ *
+ * Copyright 2015 BOXdev.com, wreckreation.org
+ */
+
+var SOUNDQ = SOUNDQ || {};
+
+
+/********************************************************************
+ * VARIABLE DECLARATIONS
+ */
+//TODO
+
+
+/********************************************************************
+ * When the page loads draw our initial script component.
+ */
+$( window ).load( function()
+{
+   //TODO
+});
