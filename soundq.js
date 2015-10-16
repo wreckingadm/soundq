6ee2c6b3dbcc03f01d511f14a936768eb4a6ee06
wreckingadm <wreckingadm@users.noreply.github.com>
2015-10-16 00:17:20 -0400

    Initial Web Audio test
    Web Audio playback on button click using sample code. Needs some
    cleanup, and auto-selection of Web Audio when available with HTML5 as a
    fallback.

--- soundq.js
+++ soundq.js
@@ -32,7 +32,95 @@ $(document).ready( function()
    });
 
 
-   $( '#audio_play' ).click( function() {
+
+   //http://creativejs.com/resources/web-audio-api-getting-started/
+   (function() {
+    var context, soundSource, soundBuffer, url = 'src/rl_short.m4a';
+
+    // Step 1 - Initialise the Audio Context
+    // There can be only one!
+
+    function init() {
+        if (typeof AudioContext !== "undefined") {
+            context = new AudioContext();
+        } else if (typeof webkitAudioContext !== "undefined") {
+            context = new webkitAudioContext();
+        } else {
+            throw new Error('AudioContext not supported. :(');
+        }
+    }
+
+    // Step 2: Load our Sound using XHR
+    function startSound() {
+        // Note: this loads asynchronously
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.responseType = "arraybuffer";
+
+        // Our asynchronous callback
+        request.onload = function() {
+            var audioData = request.response;
+
+            audioGraph(audioData);
+
+
+        };
+
+        request.send();
+    }
+
+    // Finally: tell the source when to start
+    function playSound() {
+        // play the source now
+        soundSource.start(context.currentTime);
+    }
+
+    function stopSound() {
+        // stop the source now
+        soundSource.stop(context.currentTime);
+    }
+
+    // Events for the play/stop bottons
+   //document.querySelector('.play').addEventListener('click', startSound);
+   $( '#audio_webaudio_play' ).click( function() {
+      startSound();
+   });
+
+    // This is the code we are interested in:
+    function audioGraph(audioData) {
+        soundSource = context.createBufferSource();
+        context.decodeAudioData(audioData, function(soundBuffer){
+            soundSource.buffer = soundBuffer;
+    
+            // Wiring
+            soundSource.connect(context.destination);
+    
+            // Finally
+            playSound(soundSource);
+        });
+    }
+
+    init();
+
+
+}());
+
+
+
+
+   /*
+   var ac;
+   try {
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      ac = new AudioContext();
+   }
+   catch(e) {
+      alert('Web Audio API is not supported in this browser');
+   }
+   */
+
+
+   $( '#audio_html5_play' ).click( function() {
       $( '#audio_demo' ).trigger( 'play' );
       //$( '#audio_demo' ).trigger( 'load' );
    });
